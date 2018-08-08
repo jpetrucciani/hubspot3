d13722e2839f7377f253e61d33e1f3ffeae5b5ad
Jacobi Petrucciani <jacobi@mimirhq.com>
2018-08-08 19:49:08 -0400

    adding a mock for pycurl so rtdocs can build

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,18 @@
 #
 import os
 import sys
+from unittest.mock import MagicMock
+
+
+class Mock(MagicMock):
+
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+
+MOCK_MODULES = ["pycurl"]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
 src_dir = os.path.realpath(os.path.dirname(os.path.dirname(__file__)))
