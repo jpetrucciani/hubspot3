a3581e92ef034864b8a9ffa6ab97de3f8cfc909f
Jacobi Petrucciani <j@mimir.cloud>
2019-05-30 16:45:59 -0400

    applying black styles to the new client, and removing f-string for now.

--- hubspot3/products.py
+++ hubspot3/products.py
@@ -1,13 +1,13 @@
 """
 hubspot products api
 """
-from .base import BaseClient
-from .utils import prettify
-from . import logging_helper
+from hubspot3.base import BaseClient
+from hubspot3.utils import prettify
+from hubspot3 import logging_helper
 from typing import List
 
 
-PRODUCTS_API_VERSION = '1'
+PRODUCTS_API_VERSION = "1"
 
 
 class ProductsClient(BaseClient):
@@ -16,7 +16,9 @@ class ProductsClient(BaseClient):
     THIS API ENDPOINT IS ONLY A PREVIEW AND IS SUBJECT TO CHANGE
     see: https://developers.hubspot.com/docs/methods/products/products-overview
     """
-    def __init__(self, *args, **kwargs):
+
+    def __init__(self, *args, **kwargs) -> None:
+        """initialize a products client"""
         super(ProductsClient, self).__init__(*args, **kwargs)
         self.log = logging_helper.get_log("hubspot3.companies")
 
@@ -24,20 +26,16 @@ class ProductsClient(BaseClient):
         """get single product based on product ID in the hubspot account"""
         properties = properties or []
         return self._call(
-            f'objects/products/{product_id}',
-            method='GET',
-            params={
-                'properties': [
-                    'name',
-                    'description',
-                    *properties
-                ]
-            },
+            "objects/products/{product_id}".format(product_id=product_id),
+            method="GET",
+            params={"properties": ["name", "description", *properties]},
             doseq=True,
             **options
         )
 
-    def get_all_products(self, properties: List[str] = None, offset: int = 0, **options):
+    def get_all_products(
+        self, properties: List[str] = None, offset: int = 0, **options
+    ):
         """get all products in the hubspot account"""
         properties = properties or []
         finished = False
@@ -45,31 +43,29 @@ class ProductsClient(BaseClient):
         querylimit = 100  # Max value according to docs
         while not finished:
             batch = self._call(
-                'objects/products/paged',
-                method='GET',
+                "objects/products/paged",
+                method="GET",
                 params={
-                    'limit': querylimit,
-                    'offset': offset,
-                    'properties': [
-                        'name',
-                        'description',
-                        *properties
-                    ]
+                    "limit": querylimit,
+                    "offset": offset,
+                    "properties": ["name", "description", *properties],
                 },
                 doseq=True,
                 **options
             )
-            output.extend([
-                prettify(obj, id_key='objectId')
-                for obj in batch['objects']
-                if not obj['isDeleted']
-            ])
-            finished = not batch['hasMore']
-            offset = batch['offset']
+            output.extend(
+                [
+                    prettify(obj, id_key="objectId")
+                    for obj in batch["objects"]
+                    if not obj["isDeleted"]
+                ]
+            )
+            finished = not batch["hasMore"]
+            offset = batch["offset"]
 
         return output
 
     def _get_path(self, subpath: str):
-        return 'crm-objects/v{}/{}'.format(
-            self.options.get('version') or PRODUCTS_API_VERSION, subpath
+        return "crm-objects/v{}/{}".format(
+            self.options.get("version") or PRODUCTS_API_VERSION, subpath
         )
