ee9b1788d1af9b1c62e119b7946ab847c5d26b72
Henning Kage <henning.kage@gmail.com>
2019-06-17 09:32:01 +0200

    Display only BaseClient instances or public methods within the CLI usage

--- cli.py
+++ cli.py
@@ -1,13 +1,35 @@
 """Commandline entry for the Hubspot client."""
 
-import fire
+import inspect
 
+import fire
+from fire.completion import _IncludeMember
 from hubspot3 import Hubspot3
+from hubspot3.base import BaseClient
+
+
+def filter_members(member) -> tuple:
+    """Return only public API components."""
+    name, component = member
+    if isinstance(component, BaseClient) or inspect.ismethod(component):
+        print(type(member))
+        return member
+
+
+def get_members(component, verbose: bool = False) -> list:
+    """Retrieve all members of a component."""
+    if isinstance(component, dict):
+        members = component.items()
+    else:
+        members = inspect.getmembers(component)
+    members = filter(filter_members, members)
+    return [(member_name, member) for member_name, member in members if _IncludeMember(member_name, verbose)]
 
 
 def main():
+    fire.completion._Members = get_members
     fire.Fire(Hubspot3)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
