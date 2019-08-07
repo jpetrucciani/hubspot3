8a327111b441ac076c36da1560b9ab7fd029324b
Henning Kage <henning.kage@gmail.com>
2019-08-07 16:22:03 +0200

    INC-1832: Added lines delete method

--- hubspot3/lines.py
+++ hubspot3/lines.py
@@ -1,6 +1,8 @@
 """
 hubspot lines api
 """
+from typing import Dict
+
 from hubspot3.crm_associations import CRMAssociationsClient
 from hubspot3.base import BaseClient
 from hubspot3.utils import get_log
@@ -20,19 +22,28 @@ class LinesClient(BaseClient):
         super(LinesClient, self).__init__(*args, **kwargs)
         self.log = get_log("hubspot3.lines")
 
-    def create(self, data=None, **options):
-        return self._call("", data=data, method="POST", **options)
-
-    def get(self, line_id, **options):
-        """Retrieve a line by its id."""
-        return self._call("{}".format(line_id), **options)
-
-    def link_line_item_to_deal(self, line_item_id, deal_id):
-        """Link a line item to a deal."""
-        associations_client = CRMAssociationsClient(**self.credentials)
-        return associations_client.link_line_item_to_deal(line_item_id, deal_id)
-
     def _get_path(self, subpath: str):
         return "crm-objects/v{}/objects/line_items/{}".format(
             self.options.get("version") or LINES_API_VERSION, subpath
         )
+
+    def create(self, data=None, **options) -> Dict:
+        return self._call("", data=data, method="POST", **options)
+
+    def delete(self, line_id: int, **options) -> Dict:
+        """
+        Delete a line item by its ID.
+        :see: https://developers.hubspot.com/docs/methods/line-items/delete-line-item
+        """
+        return self._call("{}".format(line_id), method="DELETE",**options)
+
+    def get(self, line_id: int, **options) -> Dict:
+        """Retrieve a line by its ID."""
+        return self._call("{}".format(line_id), **options)
+
+    def link_line_item_to_deal(self, line_item_id, deal_id) -> Dict:
+        """Link a line item to a deal."""
+        associations_client = CRMAssociationsClient(**self.credentials)
+        return associations_client.link_line_item_to_deal(line_item_id, deal_id)
+
+    
