35f141459145763d5b7666f37a877535b0f0993e
Bernardo Camargos <ber.camargos@gmail.com>
2019-10-23 17:56:04 -0300

    Added tests for ticket's recent modified

--- hubspot3/test/test_tickets.py
+++ hubspot3/test/test_tickets.py
@@ -1,7 +1,11 @@
 """
 testing hubspot3.tickets
 """
+import json
+from unittest.mock import Mock
+
 import pytest
+
 from hubspot3.tickets import TicketsClient
 from hubspot3.test.globals import TEST_KEY
 
@@ -12,6 +16,15 @@ TICKETS = TicketsClient(api_key=TEST_KEY)
 # since we need to have an id to submit and to attempt to get a ticket,
 # we need to be hacky here and fetch one upon loading this file.
 BASE_TICKET = TICKETS.get_all(limit=1)[0]
+
+
+
+
+@pytest.fixture
+def tickets_client(mock_connection):
+    client = TicketsClient(disable_auth=True)
+    client.options["connection_type"] = Mock(return_value=mock_connection)
+    return client
 
 
 def test_create_ticket():
@@ -49,3 +62,205 @@ def test_get_all_tickets():
     assert tickets
     assert isinstance(tickets, list)
     assert len(tickets) > 1
+
+
+@pytest.mark.parametrize(
+    "extra_properties_given, extra_properties_as_list",
+    [
+        (None, []),
+        ("lead_source", ["lead_source"]),
+        (
+            ["hs_analytics_last_url", "hs_analytics_revenue"],
+            ["hs_analytics_last_url", "hs_analytics_revenue"],
+        ),
+    ],
+)
+def test_get_batch(
+    tickets_client,
+    mock_connection,
+    extra_properties_given,
+    extra_properties_as_list,
+):
+    response_body = {
+        "3234574": {"objectId": 3234574, "properties": {}},
+        "3234575": {"objectId": 3234575, "properties": {}},
+    }
+    ids = ["3234574", "3234575"]
+    properties = tickets_client.default_batch_properties.copy()
+    properties.extend(extra_properties_as_list)
+    params = {"properties": p for p in properties}
+
+    mock_connection.set_response(200, json.dumps(response_body))
+    resp = tickets_client.get_batch(ids, extra_properties_given)
+    mock_connection.assert_num_requests(1)
+    mock_connection.assert_has_request(
+        "POST", "/crm-objects/v1/objects/tickets/batch-read", **params, data={'ids': ids}
+    )
+    assert len(resp) == 2
+    assert {"id": 3234574} in resp
+    assert {"id": 3234575} in resp
+
+@pytest.mark.parametrize(
+    "extra_properties_given, extra_properties_as_list",
+    [
+        (None, []),
+        ("lead_source", ["lead_source"]),
+        (
+            ["hs_analytics_last_url", "hs_analytics_revenue"],
+            ["hs_analytics_last_url", "hs_analytics_revenue"],
+        ),
+    ],
+)
+def test_get_batch_with_history(
+    tickets_client,
+    mock_connection,
+    extra_properties_given,
+    extra_properties_as_list,
+):
+    response_body = {
+        "3234574": {"objectId": 3234574, "properties": {}},
+        "3234575": {"objectId": 3234575, "properties": {}},
+    }
+    ids = ["3234574", "3234575"]
+    properties = tickets_client.default_batch_properties.copy()
+    properties.extend(extra_properties_as_list)
+    params = {"propertiesWithHistory": p for p in properties}
+
+    mock_connection.set_response(200, json.dumps(response_body))
+    resp = tickets_client.get_batch_with_history(ids, extra_properties_given)
+    mock_connection.assert_num_requests(1)
+    mock_connection.assert_has_request(
+        "POST", "/crm-objects/v1/objects/tickets/batch-read", **params, data={'ids': ids}
+    )
+    assert len(resp) == 2
+    assert '3234574' in resp
+    assert '3234575' in resp
+
+
+def base_get_recently(
+    tickets_client,
+    mock_connection,
+    changeType
+):
+    response_body_recent = [
+        {
+            'timestamp': 1571409899877,
+            'changeType': 'CHANGED',
+            'objectId': 47005994,
+            'changes':
+            {
+                'changedProperties': ['hs_lastcontacted', 'hs_last_email_activity'],
+                'newAssociations': [],
+                'removedAssociations': [],
+                'newListMemberships': [],
+                'removedListMemberships': [],
+            }
+        },
+        {
+            'timestamp': 1571411169000,
+            'changeType': 'CREATED',
+            'objectId': 47005994,
+            'changes':
+            {
+                'changedProperties': ['hs_lastcontacted', 'hs_last_email_activity'],
+                'newAssociations': [],
+                'removedAssociations': [],
+                'newListMemberships': [],
+                'removedListMemberships': [],
+            }
+        }
+    ]
+    response_body_batch = {
+        '47005994': {
+            'objectType': 'TICKET',
+            'portalId': 5282301,
+            'objectId': 47005994,
+            'properties': {
+                'hs_lastcontacted': {
+                    'versions': [{
+                        'name': 'hs_lastcontacted',
+                        'value': '1571427728000',
+                        'timestamp': 1571411169418,
+                        'sourceId': 'TicketsRollupProperties',
+                        'source': 'CALCULATED',
+                        'sourceVid': []
+                    }, {
+                        'name': 'hs_lastcontacted',
+                        'value': '1571415325000',
+                        'timestamp': 1571410899877,
+                        'sourceId': 'TicketsRollupProperties',
+                        'source': 'CALCULATED',
+                        'sourceVid': []
+                    }],
+                    'value': '1571427728000',
+                    'timestamp': 1571411169418,
+                    'source': 'CALCULATED',
+                    'sourceId': 'TicketsRollupProperties'
+                },
+                'hs_last_email_activity': {
+                    'versions': [{
+                        'name': 'hs_last_email_activity',
+                        'value': 'SENT_TO_CONTACT',
+                        'timestamp': 1571411169118,
+                        'sourceId': 'TicketsRollupProperties',
+                        'source': 'CALCULATED',
+                        'sourceVid': []
+                    }, {
+                        'name': 'hs_last_email_activity',
+                        'value': 'REPLY_FROM_CONTACT',
+                        'timestamp': 1571409899877,
+                        'sourceId': 'TicketsRollupProperties',
+                        'source': 'CALCULATED',
+                        'sourceVid': []
+                    }],
+                    'value': 'SENT_TO_CONTACT',
+                    'timestamp': 1571411169118,
+                    'source': 'CALCULATED',
+                    'sourceId': 'TicketsRollupProperties'
+                }
+            }
+        }
+    }
+    ids = [47005994]
+    properties = ['hs_lastcontacted', 'hs_last_email_activity']
+    params_batch = {"propertiesWithHistory": p for p in properties}
+    params_recent = {
+        'count': 100,
+        'objectId': 47005994,
+        'timestamp': 1571411169000,
+        'changeType': changeType
+    }
+
+    mock_connection.set_responses([(200, json.dumps(response_body_recent)),
+                                   (200, json.dumps(response_body_batch)),
+                                   (200, '[]')])
+    if changeType == 'CHANGED':
+        changes = tickets_client.get_recently_modified(time_offset=1571847002)
+    else:
+        changes = tickets_client.get_recently_created(time_offset=1571847002)
+    mock_connection.assert_num_requests(3)
+
+    mock_connection.assert_has_request(
+        "GET", "/crm-objects/v1/change-log/ticket", **params_recent
+    )
+    mock_connection.assert_has_request(
+        "POST", "/crm-objects/v1/objects/tickets/batch-read", **params_batch, data={'ids': ids},
+    )
+    assert len(changes) == 2
+    assert [47005994, 47005994] == [change['objectId'] for change in changes]
+    assert len(changes[0]['changes']['changedValues']) == 1
+    assert len(changes[1]['changes']['changedValues']) == 2
+
+
+def test_get_recently_created(
+    tickets_client,
+    mock_connection,
+):
+    base_get_recently(tickets_client, mock_connection, 'CREATED')
+
+
+def test_get_recently_modified(
+    tickets_client,
+    mock_connection,
+):
+    base_get_recently(tickets_client, mock_connection, 'CHANGED')
