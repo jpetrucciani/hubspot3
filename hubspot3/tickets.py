79ebd9192a7404c07411abc5b3257acf366e96ca
Bernardo Camargos <ber.camargos@gmail.com>
2019-11-08 16:10:19 -0300

    Changing to use dict.get()

--- hubspot3/tickets.py
+++ hubspot3/tickets.py
@@ -316,10 +316,7 @@ class TicketsClient(BaseClient):
                     min_time_index = min(range(len(time_diffs)),
                                          key=time_diffs.__getitem__)
                     if time_diffs[min_time_index] <= max_time_diff_ms:
-                        if "value" in versions[min_time_index]:
-                            value = versions[min_time_index]["value"]
-                        else:
-                            value = None
+                        value = versions[min_time_index].get("value")
                         change["changes"]["changedValues"][changed_variable] = value
                     else:
                         get_log(__name__).warning("Unable to find value for "
