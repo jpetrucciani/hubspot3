3c673c947840cf1f7a21defd1d2f1737f3c14164
Keith Bauson <kwbauson@gmail.com>
2020-10-30 11:26:58 -0400

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  mach-nix =
+    import
+      (builtins.fetchGit {
+        url = "https://github.com/DavHau/mach-nix/";
+        ref = "refs/tags/3.0.2";
+      }) { };
+in
+mach-nix.mkPythonShell {
+  requirements = ''
+    ${builtins.readFile ./requirements.txt}
+    ${builtins.readFile ./requirements-dev.txt}
+  '';
+}
