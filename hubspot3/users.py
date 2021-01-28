24dff3abe6e830c3db77d365d20e316b5527d0ce
Evan Sangaline <esangaline@gmail.com>
2021-01-28 07:23:24 -0800

    Add a method for getting a user by ID.

--- hubspot3/users.py
+++ hubspot3/users.py
@@ -1,6 +1,8 @@
 """
 hubspot users api
 """
+from typing import Union
+
 from hubspot3.base import BaseClient
 
 USERS_API_VERSION = "v3"
@@ -15,6 +17,10 @@ class UsersClient(BaseClient):
     def _get_path(self, subpath: str):
         """Get the full api url for the given subpath on this client."""
         return "settings/{}/users/{}".format(USERS_API_VERSION, subpath)
+
+    def get_by_id(self, user_id: Union[int, str], **options):
+        """Get user specified by ID."""
+        return self._call(str(user_id), method="GET", **options)
 
     def get_roles(self, **options):
         """Get all existing user roles."""
