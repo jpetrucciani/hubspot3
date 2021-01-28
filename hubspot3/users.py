9139235a84e6050fffecb64890562795c8fce8bd
Evan Sangaline <esangaline@gmail.com>
2021-01-28 07:31:42 -0800

    Add a method to create a new user.

--- hubspot3/users.py
+++ hubspot3/users.py
@@ -18,6 +18,22 @@ class UsersClient(BaseClient):
         """Get the full api url for the given subpath on this client."""
         return "settings/{}/users/{}".format(USERS_API_VERSION, subpath)
 
+    def create(
+        self, email: str, role_id: Union[int, str], send_welcome_email: bool = False, **options
+    ):
+        """Create a new user with minimal contacts-base permissions."""
+        data = {
+            "email": email,
+            "roleId": str(role_id),
+            "sendWelcomeEmail": send_welcome_email,
+        }
+        return self._call(
+            "",
+            data=data,
+            method="POST",
+            **options,
+        )
+
     def get_by_id(self, user_id: Union[int, str], **options):
         """Get user specified by ID."""
         return self._call(str(user_id), method="GET", **options)
