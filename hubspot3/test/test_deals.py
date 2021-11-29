76126cc3585ce0495a93dbe6bd6bbbd6fac00a87
victor.urquiola <victor.urquiola@cobli.co>
2021-11-29 13:26:23 -0300

    arrumando o arquivo com as sugestoes da isa

--- hubspot3/test/test_deals.py
+++ hubspot3/test/test_deals.py
@@ -2,19 +2,19 @@
 Testing hubspot3.deals
 This file tests the methods in the hubspot3.deals file
 """
+import json
+from unittest.mock import Mock
 import pytest
-from hubspot3.deals import DealsClient
 from hubspot3.error import HubspotNotFound, HubspotBadRequest
 from hubspot3.test.globals import TEST_KEY
 from hubspot3 import deals
-import json
-from unittest.mock import Mock
+
 
 deals_client = deals.DealsClient(api_key=TEST_KEY)
 
 
 @pytest.fixture
-def deals_client(mock_connection):
+def mock_deals_client(mock_connection):
     client = deals.DealsClient(disable_auth=True)
     client.options["connection_type"] = Mock(return_value=mock_connection)
     return client
@@ -36,9 +36,9 @@ def test_get_deal():
     :see: https://developers.hubspot.com/docs/methods/deals/get_deal
     """
     with pytest.raises(HubspotNotFound):
-        DEALS.get("-1")
+        deals_client.get("-1")
 
-    deal_check = DEALS.get("852832561")
+    deal_check = deals_client.get("852832561")
     assert _is_deal(deal_check["properties"])
 
 
@@ -48,7 +48,7 @@ def test_create_deal():
     :see: https://developers.hubspot.com/docs/methods/deals/create_deal
     """
     with pytest.raises(HubspotBadRequest):
-        DEALS.create()
+        deals_client.create()
 
 
 def test_delete_deal():
@@ -62,11 +62,11 @@ def test_delete_deal():
             {"value": None, "name": "createdate"},
         ]
     }
-    response_data = DEALS.create(data)
+    response_data = deals_client.create(data)
     deal_id = response_data["dealId"]
-    DEALS.delete(deal_id)
+    deals_client.delete(deal_id)
     with pytest.raises(HubspotNotFound):
-        DEALS.get(deal_id)
+        deals_client.get(deal_id)
 
 
 def test_get_recently_created():
@@ -74,7 +74,7 @@ def test_get_recently_created():
     gets recently created deals
     :see: https://developers.hubspot.com/docs/methods/deals/get_deals_created
     """
-    new_deals = DEALS.get_recently_created(limit=20)
+    new_deals = deals_client.get_recently_created(limit=20)
     # assert new_deals
     assert len(new_deals) <= 20
     # assert _is_deal(new_deals[0])
@@ -93,7 +93,7 @@ def test_get_recently_modified():
 
 @pytest.mark.parametrize("start_date, end_date", [(20, 40)])
 def test_get_recently_modified_in_interval(
-    deals_client,
+    mock_deals_client,
     mock_connection,
     start_date,
     end_date,
@@ -157,7 +157,7 @@ def test_get_recently_modified_in_interval(
         ]
     }
     mock_connection.set_response(200, json.dumps(response_body))
-    modified_deals = list(deals_client.get_recently_modified_in_interval(start_date, end_date))
+    modified_deals = list(mock_deals_client.get_recently_modified_in_interval(start_date, end_date))
     mock_connection.assert_num_requests(1)
     mock_connection.assert_has_request(
         "GET", "/deals/v1/deal/recent/modified", **params
